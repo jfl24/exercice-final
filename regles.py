03eb2e70f602dbc7256aa179524350e6f31fe8bd
Samuel Lortie <samuellortie2001@gmail.com>
2025-12-19 16:18:55 -0500

    developement de la fonction definir_gagnant

--- regles.py
+++ regles.py
@@ -1,2 +1,7 @@
 def definir_gagnant(joueur, ordi):
-    pass
+    if joueur == ordi:
+        return "Match Nul!"
+    if (joueur == "roche" and ordi == "ciseaux") or (joueur == "ciseaux" and ordi == "papier") or (joueur == "papier" and ordi == "roche"):
+        return "Vous avez gagné!"
+    else:
+        return "Vous avez perdu!"
